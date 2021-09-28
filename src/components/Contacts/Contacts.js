d21d2ac6a100aa2405dab4bf09b087beced1f922
SokolMks <sokolenkomks@gmail.com>
2021-09-28 15:15:16 +0300

    Added code not everything works

---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { deleteContact } from "../../redux/actions";
+import PropTypes from "prop-types";
+import style from "./Contacts.module.css";
+
+const Contacts = () => {
+  const dispatch = useDispatch();
+  const onDeleteContact = (id) => dispatch(deleteContact(id));
+
+  const getContactList = (state) => {
+    const { filter, items } = state.contacts;
+    const normalizedFilter = filter.toLowerCase();
+
+    return items.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+  const contacts = useSelector(getContactList);
+
+  return (
+    <div className={style.container}>
+      <h2 className={style.title}>Contacts</h2>
+        <ul className={style.list}>
+          <label className={style.label}>
+            Find contacts by name:
+            <input
+              type="text"
+              name="name"
+              //value={filterContacts}
+              //onChange={onChangeFilter}
+              //pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              //title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              required
+            />
+          </label>
+          {contacts.map(({ id, name, number }) => (
+            <li key={id} className={style.listElement}>
+              <p>
+                {name}: {number}
+              </p>
+              <button className={style.btn} onClick={() => onDeleteContact(id)}>
+                Delete contact
+              </button>
+            </li>
+          ))}
+        </ul>
+
+    </div>
+  );
+};
+
+Contacts.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+export default Contacts;
---
+++ src/components/Phonebook/Phonebook.js
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addContact } from "../../redux/actions";
+import PropTypes from "prop-types";
+import style from "./Phonebook.module.css";
+
+function PhoneBook({ submitForm }) {
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+
+  const handleChangeInput = (e) => {
+    const { value } = e.currentTarget;
+
+    if (e.target.name === "name") {
+      setName(value);
+    }
+    if(e.target.name === "number") {
+      setNumber(value);  
+    }
+  };
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+
+    //submitForm({ name, number });
+    dispatch(addContact({ name, number }));
+    formReset();
+  };
+
+  const formReset = () => {
+    setName("");
+    setNumber("");
+  };
+
+  return (
+    <form className={style.containerForm} onSubmit={onSubmitForm}>
+      <h2 className={style.title}>Phonebook</h2>
+      <label>
+        Name
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChangeInput}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+        />
+      </label>
+      <label>
+        Number
+        <input
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleChangeInput}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+          required
+        />
+      </label>
+      <button className={style.btn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+}
+
+PhoneBook.propTypes = {
+  submitForm: PropTypes.func.isRequired,
+};
+
+export default PhoneBook;
