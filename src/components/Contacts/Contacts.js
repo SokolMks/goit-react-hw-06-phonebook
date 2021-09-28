02ada125e7c5959a8f82ea438bbb5ece99aa666c
SokolMks <sokolenkomks@gmail.com>
2021-09-28 19:04:43 +0300

    code refactoring

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -22,18 +22,6 @@ const Contacts = () => {
     <div className={style.container}>
       <h2 className={style.title}>Contacts</h2>
         <ul className={style.list}>
-          {/* <label className={style.label}>
-            Find contacts by name:
-            <input
-              type="text"
-              name="name"
-              //value={filterContacts}
-              //onChange={onChangeFilter}
-              //pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              //title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-              required
-            />
-          </label> */}
           {contacts.map(({ id, name, number }) => (
             <li key={id} className={style.listElement}>
               <p>
